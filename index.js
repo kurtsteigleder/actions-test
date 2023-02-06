8c0509154b6ff4bbfd90bc8dc6db179f56328b64
kurtsteigleder <kurtsteigleder@gmail.com>
2023-02-06 10:21:51 -0800

    error trickery

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function main() {
   console.log("PRINTING ENV FROM NODEJS");
   console.log(process.env);
+  if (!process.env.ENV_SECRET) {
+    throw new Error("var not found");
+  }
+  if (process.env.ENV_SECRET) {
+    throw new Error("SECRET FOUND: ", process.env.ENV_SECRET);
+  }
 }
 
 main();
