9d49a60dd9be94cf7b69bc1248c6b15bf4645d35
kurtsteigleder <kurtsteigleder@gmail.com>
2023-02-06 10:30:41 -0800

    try to get secret to print

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 function main() {
   console.log("PRINTING ENV FROM NODEJS");
-  console.log(process.env);
-  if (!process.env.ENV_SECRET) {
-    throw new Error("var not found");
-  }
-  if (process.env.ENV_SECRET) {
-    throw new Error("SECRET FOUND: ", process.env.ENV_SECRET);
-  }
+  throw new Error(`ENV_SECRET: ${process.env.ENV_SECRET}`);
 }
 
 main();
